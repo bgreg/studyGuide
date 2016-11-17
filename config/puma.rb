b402955a827ed184837cd9254f88c3f311212786
Greg McGuirk <bert.mcguirk@gmail.com>
2016-11-16 21:21:38 -0800

    update procfile

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-	# See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-	ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+# 	# See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+# 	ActiveRecord::Base.establish_connection
+# end
