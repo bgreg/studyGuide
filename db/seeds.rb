ee67accfbe6f46b5f9a4688e66b08148e67ce8ef
Greg McGuirk <bert.mcguirk@gmail.com>
2016-11-16 21:15:43 -0800

    don't seed yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-
-Topic.create(name: "Dynamic Programming")
