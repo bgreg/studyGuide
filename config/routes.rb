a7383d9d8066c0c926cbdde8c86dc355c503bd89
Greg McGuirk <bert.mcguirk@gmail.com>
2016-11-16 20:42:17 -0800

    add rspec and update topics page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root to: "topics#index"
+
   devise_for :accounts
   namespace :admin do
     resources :examples
-resources :links
-resources :topics
-
-    root to: "examples#index"
+    resources :links
+    resources :topics
   end
 
   resources :topics do
