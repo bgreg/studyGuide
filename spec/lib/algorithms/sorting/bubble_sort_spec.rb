0238e4e9da6bc79cf7d06db516040b0b37695274
Greg McGuirk <bert.mcguirk@gmail.com>
2016-12-17 11:56:12 -0800

    add bubble sort example script

---
+++ spec/lib/algorithms/sorting/bubble_sort_spec.rb
@@ -0,0 +1,66 @@
+require_relative "../../../../lib/algorithms/sorting/bubble_sort"
+require "spec_helper"
+
+describe BubbleSort do
+  describe "#sort" do
+    context "Organizing arrays into numerical order is helpful when performing searching operations. "\
+            "Bubble sort can arrange any array into acending or decending order by simply repeatedly switching "\
+            "places with it's nearest neighbor. Called 'Bubble Sort' because outliers sink to the ends of the "\
+            "list quickly and medium values slowly raise like bubbles to their final postions." do
+
+      let(:randomized_array) do
+        [
+          73,
+          82,
+          54,
+          7,
+          88,
+          11,
+          8,
+          93,
+          99,
+          74,
+          18,
+          2,
+          90,
+          80,
+          93,
+          42,
+          36,
+          31,
+          19,
+          71
+        ]
+      end
+
+      it "Sorts this random set into acending orders." do
+        bubble_sort = BubbleSort.new(random_list: randomized_array, verbose: true)
+        expect(bubble_sort.sort).to match_array(
+          [
+            2,
+            7,
+            8,
+            11,
+            18,
+            19,
+            31,
+            36,
+            42,
+            54,
+            71,
+            73,
+            74,
+            80,
+            82,
+            88,
+            90,
+            93,
+            93,
+            99,
+          ]
+        )
+      end
+
+    end
+  end
+end
