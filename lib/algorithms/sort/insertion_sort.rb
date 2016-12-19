16847a99b1d1669b8179410a745ec1e75eda6267
Greg McGuirk <bert.mcguirk@gmail.com>
2016-12-18 22:37:06 -0800

    add logging to better visualize what the data is doing

--- lib/algorithms/sort/insertion_sort.rb
+++ lib/algorithms/sort/insertion_sort.rb
@@ -2,25 +2,74 @@ require_relative "sort"
 
 class InsertionSort < Sort
   def sort
-    puts "\n" if @verbose
-
-    1.upto(@last_index).each do |index|
-      puts "@list: #{@list}" if @verbose
+    sorted_zone.each do |index|
       key = @list[index]
-      puts "key: #{key}" if @verbose
-      j = index - 1
+      print_search_start(key, index) if @verbose
 
-      while j >= 0 && @list[j] > key
-        puts "replacing #{@list[j + 1]} with #{@list[j]}" if @verbose
-        @list[j + 1] = @list[j]
-        j -= 1
+      comparator = index - 1
+      puts "First comparator set to: #{@list[comparator]}"
+      while comparator >= 0 && @list[comparator] > key
+        puts "#{@list[comparator]} is greater than #{key}"
+        print_search_compare(comparator, comparator + 1) if @verbose
+        @list[comparator + 1] = @list[comparator]
+        comparator -= 1
       end
 
-      @list[j + 1] = key
-      puts "\n" if @verbose
+      @list[comparator + 1] = key
+      print_key_position_found_message(comparator, key)
     end
 
-    puts "@list: #{@list}" if @verbose
+    print_sort_finished_message
     @list
+  end
+
+  private
+
+  def sorted_zone
+    1.upto(@last_index)
+  end
+
+  #
+  # Only printing functions down here
+  #
+
+  def print_search_start(key, index)
+    puts "-" * 120
+    puts "Already sorted zone:"
+    puts "[#{@list[0..index - 1].join(" ")}]"
+    puts
+    puts "Key: #{key}"
+    puts_key_pointer(index, show_list: true)
+  end
+
+  def print_search_compare(needle, comparator)
+    puts "Replacing #{@list[needle]} with #{@list[comparator]}"
+    puts_key_pointer(comparator, show_list: true)
+    puts_key_pointer(needle, show_list: false)
+  end
+
+  def print_key_position_found_message(comparator, key)
+    puts "Final position for key #{key} found"
+    puts_key_pointer(comparator + 1, show_list: true, pointer: "^")
+  end
+
+  def puts_key_pointer(index, show_list: true, pointer: "^")
+    display_list = @list.dup
+
+    offset =
+      if display_list[index].to_s.length > 1
+        " " * (display_list[index].to_s.length - 1)
+      else
+        ""
+      end
+
+    display_list[index] = pointer + offset
+    puts "[#{@list.join(" ")}]" if show_list
+    puts " #{display_list.join(" ").gsub(/\d/, " ")} "
+  end
+
+  def print_sort_finished_message
+    puts "Final list"
+    puts "[#{@list.join(" ")}]" if @verbose
   end
 end
